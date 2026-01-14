c1e9e8917ec282c0cc287f7a8fd6805557fc3132
youssef <youssefelghamour@gmail.com>
2026-01-14 12:54:12 +0100

    Deployment settings

--- alx_travel_app/alx_travel_app/settings.py
+++ alx_travel_app/alx_travel_app/settings.py
@@ -25,13 +25,13 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-jve(2gam@#ac+e5=osg#4-pcyaoflbtqr-8ho6pbk2qbcr!$=f'
+# Keep the secret key used in production secret
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "fallback-secret")
 
-# SECURITY WARNING: don't run with debug turned on in production!
+# Debug turned on in production
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["youssefelghamour.pythonanywhere.com"]
 
 
 # Application definition
@@ -89,6 +89,14 @@ WSGI_APPLICATION = 'alx_travel_app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
@@ -99,6 +107,7 @@ DATABASES = {
         'PORT': env('DB_PORT', default='3306'),
     }
 }
+"""
 
 
 # Password validation
